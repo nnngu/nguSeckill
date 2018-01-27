f1554f9b749c13cb66d3969a46b85e42a75e21f8
nnngu <654015592@qq.com>
2018-01-28 00:03:46 +0800

    添加实体类以及Dao接口

---
+++ src/main/java/com/nnngu/dao/SeckillMapper.java
@@ -0,0 +1,42 @@
+package com.nnngu.dao;
+
+import com.nnngu.entity.Seckill;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public interface SeckillMapper {
+    /**
+     * 根据传过来的<code>seckillId</code>去减少商品的库存.
+     *
+     * @param seckillId 秒杀商品ID
+     * @param killTime  秒杀的精确时间
+     * @return 如果秒杀成功就返回1，否则就返回0
+     */
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") LocalDateTime killTime);
+
+    /**
+     * 根据传过来的<code>seckillId</code>去查询秒杀商品的详情.
+     *
+     * @param seckillId 秒杀商品ID
+     * @return 对应商品ID的的数据
+     */
+    Seckill queryById(@Param("seckillId") long seckillId);
+
+    /**
+     * 根据一个偏移量去查询秒杀的商品列表.
+     *
+     * @param offset 偏移量
+     * @param limit  限制查询的数据个数
+     * @return 符合偏移量查出来的数据个数
+     */
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
+    /**
+     *  使用储存过程执行秒杀
+     * @param paramMap
+     */
+    void killByProcedure(Map<String,Object> paramMap);
+}
---
+++ src/main/java/com/nnngu/dao/SuccessKilledMapper.java
@@ -0,0 +1,24 @@
+package com.nnngu.dao;
+
+import com.nnngu.entity.SuccessKilled;
+import org.apache.ibatis.annotations.Param;
+
+public interface SuccessKilledMapper {
+    /**
+     * 插入一条详细的购买信息.
+     *
+     * @param seckillId 秒杀商品的ID
+     * @param userPhone 购买用户的手机号码
+     * @return 成功插入就返回1, 否则就返回0
+     */
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+
+    /**
+     * 根据秒杀商品的ID查询<code>SuccessKilled</code>的明细信息.
+     *
+     * @param seckillId 秒杀商品的ID
+     * @param userPhone 购买用户的手机号码
+     * @return 秒杀商品的明细信息
+     */
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+}
